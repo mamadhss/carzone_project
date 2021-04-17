a16f761a3fca403bb5a0de27da0ecd4abe500b02
mohammad <58949667+mamadhss@users.noreply.github.com>
2021-04-17 15:44:56 +0430

    Update settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -148,8 +148,4 @@ MESSAGE_TAGS = {
 SITE_ID = 1
 
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'carzone.project@gmail.com'
-EMAIL_HOST_PASSWORD = 'moh4mm4d580'
-EMAIL_USE_TLS = True
+
